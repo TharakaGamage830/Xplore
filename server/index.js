3f44fb42b4cd1ca8016be274ceaf61a687c70006
TharakaGamage830 <tharakaashen830@gmail.com>
2026-03-09 17:21:02 +0530

    feat: connect mongodb atlas to express backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('DB Error:', err))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Xplore API is running' })
