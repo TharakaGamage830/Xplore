61c09a8dedb366d26931486f559c5425125c2175
TharakaGamage830 <tharakaashen830@gmail.com>
2026-03-09 19:46:48 +0530

    feat: add Listing  model

---
+++ server/models/Listing.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    default: null
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Listing', listingSchema)
